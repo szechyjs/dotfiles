80fcdd0e07b660bfbd2a940272c2072e0c76fad2
Jared Szechy <jared.szechy@gmail.com>
2012-07-16 13:35:35 -0400

    Add bash and system config

---
+++ system/aliases.bash
@@ -0,0 +1,5 @@
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
---
+++ system/path.bash
@@ -0,0 +1,4 @@
+if [ -d "$HOME/android/android-sdk-linux" ] ; then
+    PATH="$PATH:$HOME/android/android-sdk-linux/tools"
+fi
+
