99cd2eddea98a62735eddaef1ea9c6feca2c96d6
Jared Szechy <jared.szechy@gmail.com>
2012-08-02 14:32:33 -0400

    Use PS1 constants for easy adjustment of PS1

--- bash/config.bash
+++ bash/config.bash
@@ -42,10 +42,12 @@ if [ -n "$force_color_prompt" ]; then
     fi
 fi
 
+source $DOTFILES/bash/ps1constants.sh
+
 if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1="${debian_chroot:+($debian_chroot)}$BGreen$User@$Host$Color_Off:$BBlue$PathShort$Color_Off __git_ps1 \$ "
 else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+    PS1="${debian_chroot:+($debian_chroot)}$User@$Host:$PathShort\$ "
 fi
 unset color_prompt force_color_prompt
 
