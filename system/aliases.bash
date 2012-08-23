6530de0ca658d2c848bb4cd55371f40eae078702
Jared Szechy <jared.szechy@gmail.com>
2012-08-22 22:03:55 -0400

    Add alias for rsync archive.

--- system/aliases.bash
+++ system/aliases.bash
@@ -6,3 +6,5 @@ alias lt='ls -lt'
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 alias grep='grep --exclude-dir=".svn" -n --color'
+
+alias archive='rsync -ahP --delete'
