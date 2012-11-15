6343647e57aaebb5629727ed6e03940559445b58
Jared Szechy <jared.szechy@gmail.com>
2012-11-15 12:03:35 -0500

    Detect any bsi network

--- system/bsi.bash
+++ system/bsi.bash
@@ -1,4 +1,4 @@
-if grep -q "oh.bsi.local" /etc/resolv.conf ; then
+if grep -q "bsi.local" /etc/resolv.conf ; then
     alias tools='source /tools/new_tools_lin.sh prod64'
     alias tools-static='source /tools/new_tools_lin.sh static-prod64'
     alias ecl-cmake='cmake -G "Eclipse CDT4 - Unix Makefiles" -D ECLIPSE_CDT4_GENERATE_SOURCE_PROJECT=TRUE -D CMAKE_BUILD_TYPE=Debug -D CMAKE_ECLIPSE_MAKE_ARGUMENTS=-j4 ../source/'
