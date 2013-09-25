beb8deb7742758939b4bede25e3b3a58cb74c00c
Jared Szechy <jared.szechy@brilligent.com>
2013-09-25 11:02:43 -0400

    update kiln tools

--- hg/kilnpath.py
+++ hg/kilnpath.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2009-2011 Fog Creek Software.  All rights reserved.
+# Copyright (C) 2009-2013 Fog Creek Software.  All rights reserved.
 #
 # To enable the "kilnpath" extension put these lines in your ~/.hgrc:
 #    [extensions]
@@ -18,7 +18,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-'''Lets users identify repository paths by custom schemes like kiln://Project/Group/Repository.
+'''allow users identify repository paths by custom schemes like kiln://Project/Group/Repository (DEPRECATED)
 
 This extensions knows how to expand Kiln repository paths from
 kiln://Project/Group/Repository to
@@ -31,10 +31,36 @@ This extensions works for pull, push, incoming, outgoing, and clone.
 To specify the Kiln path prefix, add a [kiln_scheme] section to your hg config like so:
 [kiln_scheme]
 kiln = https://your.fogbugz.com/kiln/Repo
+
+This extensions is deprecated, and does not work in Mercurial 2.4 and later.
+You should use the built-in `schemes` extension instead instead.  Type `hg help schemes`
+for more information.
 '''
 
-import os
-from mercurial import extensions, commands, hg, ui
+import re
+
+from mercurial import hg, ui, util, __version__
+
+
+@apply
+def _HG_VERSION():
+    '''return the mercurial version as a tuple rather than a string
+
+    Python does the right thing when comparing tuples, so the return
+    value can be used to compare and detect versions.
+    '''
+    version = [0, 0, 0]
+    parts = [re.match(r'\d+', v).group(0) for v in __version__.version.split('.')[:3]]
+    for i, part in enumerate(map(int, parts)):
+        version[i] = part
+    return tuple(version)
+
+
+if _HG_VERSION >= (2, 3, 0):
+    raise util.Abort(
+        'kilnpath is deprecated, and does not work in Mercurial 2.3 or higher.  '
+        'Use the official schemes extension instead')
+
 
 def urlcombine(pre, suff):
     if pre.endswith("/"):
@@ -43,10 +69,12 @@ def urlcombine(pre, suff):
         suff = "/" + suff
     return pre + suff
 
+
 def add_schemes(ui):
     schemes = ui.configitems('kiln_scheme')
     for scheme, prefix in schemes:
         hg.schemes[scheme] = KilnRepo(scheme, prefix)
+
 
 class KilnRepo(object):
     def __init__(self, scheme, prefix):
@@ -70,9 +98,9 @@ class KilnRepo(object):
             # Mercurial <= 1.8
             return hg._lookup(path_full).instance(ui, path_full, create)
 
+
 def extsetup(*args):
     if len(args) > 0:
         add_schemes(args[0])
     else:
         add_schemes(ui.ui())
-
