df6b1ea2122dec85acd277ba9f84a0c60a42e939
Jared Szechy <jared.szechy@gmail.com>
2014-03-07 03:33:28 -0500

    Add atom as new default editor

--- system/env.bash
+++ system/env.bash
@@ -1,4 +1,6 @@
-if type -p subl > /dev/null ; then
+if type -p atom > /dev/null ; then
+    export EDITOR=atom
+elif type -p subl > /dev/null ; then
     export EDITOR=subl
 else
     export EDITOR=vim
