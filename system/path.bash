6ec11cdc2ad3ba2ec68c730bd024400ccaac1bb4
Jared Szechy <jared.szechy@gmail.com>
2020-04-14 00:18:32 -0400

    sbin should be at beginning of path

--- system/path.bash
+++ system/path.bash
@@ -1,3 +1,7 @@
+if [ -d "/usr/local/sbin" ] ; then
+    export PATH="/usr/local/sbin:$PATH"
+fi
+
 if [ -d "$HOME/android/android-sdk-linux" ] ; then
     export PATH="$PATH:$HOME/android/android-sdk-linux/tools"
 fi
@@ -10,9 +14,6 @@ if [ -d "/usr/local/heroku/bin" ] ; then
     export PATH="$PATH:/usr/local/heroku/bin"
 fi
 
-if [ -d "/usr/local/sbin" ] ; then
-    export PATH="$PATH:/usr/local/sbin"
-fi
 
 if [ -d "$GOPATH/bin:" ] ; then
     export PATH="$PATH:$GOPATH/bin"
