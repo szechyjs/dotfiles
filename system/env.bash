9f293bd3b85f1aca7e8ee3b9f4ddec5b7de7e0b4
Jared Szechy <jared.szechy@gmail.com>
2024-02-24 20:42:55 -0500

    more zsh config

--- system/env.bash
+++ system/env.bash
@@ -3,3 +3,7 @@ export ANDROID_SDK_ROOT=/usr/local/android-sdk
 
 export STARSHIP_CONFIG=~/.starship
 
+if [ -x "$(command -v colima)" ]; then
+  export DOCKER_HOST=unix://${HOME}/.colima/default/docker.sock
+fi
+
