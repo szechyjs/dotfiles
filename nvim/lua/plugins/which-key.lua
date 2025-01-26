21da115da972c1d2bccf4f9089d0ec925e2fd8a2
Jared Szechy <jared.szechy@gmail.com>
2025-01-25 21:10:58 -0500

    Fix which-key

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,20 +1,21 @@
 return {
   "folke/which-key.nvim",
+  event = "VeryLazy",
   opts = {
-    defaults = {
-      ['g'] = { name = "+goto" },
-      [']'] = { name = '+next' },
-      ['['] = { name = '+prev' },
-      ['<leader>b'] = { name = '+buffer' },
-      ['<leader>c'] = { name = '+code' },
-      ['<leader>f'] = { name = '+find' },
-      ['<leader>g'] = { name = '+goto' },
-      -- ['<leader>n'] = { name = '+test' },
-    },
+    preset = "modern",
+    specs = {
+      { "<leader>b", group = "buffer" },
+      { "<leader>c", group = "code" },
+      { "<leader>f", group = "find" },
+      { "<leader>g", group = "goto" },
+      { "<leader>n", group = "test" },
+      { "[", group = "prev" },
+      { "]", group = "next" },
+    }
   },
   config = function(_, opts)
     local wk = require("which-key")
     wk.setup(opts)
-    wk.register(opts.defaults)
+    wk.add(opts.specs)
   end
 }
