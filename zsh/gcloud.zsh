e62b5f8b42caf948f20838b45eac149d863cd01d
Jared Szechy <jared.szechy@gmail.com>
2025-03-25 10:42:22 -0400

    zsh gcloud

---
+++ zsh/gcloud.zsh
@@ -0,0 +1,5 @@
+if [ -d "$(brew --prefix)/share/google-cloud-sdk" ]; then
+  source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
+  source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
+  alias gcurl='curl -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json"'
+fi
