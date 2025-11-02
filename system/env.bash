90db06373286d9ebbc59b3ee0f1bd85326c029e3
Jared Szechy <jared.szechy@gmail.com>
2025-11-02 00:26:54 -0400

    Add more files

--- system/env.bash
+++ system/env.bash
@@ -1,9 +1,6 @@
 export EDITOR=nvim
 export ANDROID_SDK_ROOT=/opt/android-sdk
 
-export STARSHIP_CONFIG=~/.starship
-
 if [ -x "$(command -v colima)" ]; then
   export DOCKER_HOST=unix://${HOME}/.colima/default/docker.sock
 fi
-
