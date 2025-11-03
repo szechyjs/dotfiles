980490a60f278c4346b7a5cbc150f6df260730d4
Jared Szechy <jared.szechy@gmail.com>
2025-11-03 13:23:18 -0500

    theme fixes

--- system/env.bash
+++ system/env.bash
@@ -1,5 +1,6 @@
 export EDITOR=nvim
 export ANDROID_SDK_ROOT=/opt/android-sdk
+export EZA_CONFIG_DIR=$HOME/.config/eza
 
 if [ -x "$(command -v colima)" ]; then
   export DOCKER_HOST=unix://${HOME}/.colima/default/docker.sock
