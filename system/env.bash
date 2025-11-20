f667e9224e7a9a3ccd2c4e6ce0c913d1e7111b0b
Jared Szechy <jared.szechy@gmail.com>
2025-11-20 10:58:29 -0500

    Don't default the colima host, let colima manage it

--- system/env.bash
+++ system/env.bash
@@ -1,7 +1,3 @@
 export EDITOR=nvim
 export ANDROID_SDK_ROOT=/opt/android-sdk
 export EZA_CONFIG_DIR=$HOME/.config/eza
-
-if [ -x "$(command -v colima)" ]; then
-  export DOCKER_HOST=unix://${HOME}/.colima/default/docker.sock
-fi
